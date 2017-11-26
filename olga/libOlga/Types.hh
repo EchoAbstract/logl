c6066865436784d3d267e0a49b3cf26a8b4ca441
Brian Wilson <bwilson@oblong.com>
2017-11-26 09:30:43 -0500

    Missed two files on the last commit

--- olga/libOlga/Types.hh
+++ olga/libOlga/Types.hh
@@ -1,14 +1,23 @@
 #ifndef TYPES_HH
 #define TYPES_HH
 
-using Size = size_t;
+#include <cstddef>
+#include <cstdint>
+#include <string>
 
-using U32 = uint32_t;
-using U64 = uint64_t;
-using I32 = int32_t;
-using I64 = int64_t;
+using Size = std::size_t;
+
+using U32 = std::uint32_t;
+using U64 = std::uint64_t;
+using I32 = std::int32_t;
+using I64 = std::int64_t;
 
 using Float = float;
 using Double = double;
+
+using GLHandle = U32;
+using GLLocation = I32;
+
+using String = std::string;
 
 #endif
